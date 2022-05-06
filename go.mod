eac374115690c190bcd69cd4a3d6926389bd7f39
frediet1y <zz.freedom.uver1y@gmail.com>
2022-05-06 16:49:04 +0900

    ブロックチェーンアドレスを作成

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module goblockchain
 
 go 1.18
+
+require (
+	github.com/btcsuite/btcutil v1.0.2 // indirect
+	golang.org/x/crypto v0.0.0-20220427172511-eb4f295cb31f // indirect
+)
--- wallet/wallet.go
+++ wallet/wallet.go
@@ -4,19 +4,55 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/sha256"
 	"fmt"
+
+	"github.com/btcsuite/btcutil/base58"
+	"golang.org/x/crypto/ripemd160"
 )
 
 type Wallet struct {
-	privateKey *ecdsa.PrivateKey
-	publicKey  *ecdsa.PublicKey
+	privateKey        *ecdsa.PrivateKey
+	publicKey         *ecdsa.PublicKey
+	blockchainAddress string
 }
 
 func NewWallet() *Wallet {
+	// 1. ECDSAで秘密鍵(32 bytes)・公開鍵(64 bytes)を生成
 	w := new(Wallet)
 	privateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	w.privateKey = privateKey
 	w.publicKey = &privateKey.PublicKey
+	// 2. 公開鍵をハッシュ関数SHA-256に通しハッシュ値(32 bytes)を得る
+	h2 := sha256.New()
+	h2.Write(w.publicKey.X.Bytes())
+	h2.Write(w.publicKey.Y.Bytes())
+	digest2 := h2.Sum(nil)
+	// 3. 手順2のハッシュ値をハッシュ関数RIPEMD-160に通し(20 bytes)を得る
+	h3 := ripemd160.New()
+	h3.Write(digest2)
+	digest3 := h3.Sum(nil)
+	// 4. ハッシュ値の先頭にプレフィックスとして00を加える(メインネットワーク用であることを明示)
+	vd4 := make([]byte, 21)
+	vd4[0] = 0x00
+	copy(vd4[1:], digest3[:])
+	// 5. ハッシュ関数SHA-256に通す
+	h5 := sha256.New()
+	h5.Write(vd4)
+	digest5 := h5.Sum(nil)
+	// 6. もう一度ハッシュ関数SHA-256に通す
+	h6 := sha256.New()
+	h6.Write(digest5)
+	digest6 := h6.Sum(nil)
+	// 7. 最初の4バイト分をチェックサム用として取得する
+	chsum := digest6[:4]
+	// 8.  手順4で取得した値の後ろにチェックサムを加える
+	dc8 := make([]byte, 25)
+	copy(dc8[:21], vd4[:])
+	copy(dc8[21:], chsum[:])
+	// 9. Base58のフォーマットでエンコーディングする
+	address := base58.Encode(dc8)
+	w.blockchainAddress = address
 	return w
 }
 
@@ -34,4 +70,8 @@ func (w *Wallet) PublicKey() *ecdsa.PublicKey {
 
 func (w *Wallet) PublicKeyStr() string {
 	return fmt.Sprintf("%x%x", w.publicKey.X.Bytes(), w.publicKey.Y.Bytes())
+}
+
+func (w *Wallet) BlockchainAddress() string {
+	return w.blockchainAddress
 }
